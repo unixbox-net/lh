b3c170707a577e090664bf378380ff2ef84e3d0d
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-09 20:56:25 -0700

    Update json_export.hpp

--- json_export.hpp
+++ json_export.hpp
@@ -1,6 +1,7 @@
+// json_export.hpp
 #ifndef JSON_EXPORT_HPP
 #define JSON_EXPORT_HPP
 
-void export_search_results_to_json(const char *log_search_path);
+void export_search_results_to_json(const char* log_search_path);
 
-#endif
+#endif // JSON_EXPORT_HPP
