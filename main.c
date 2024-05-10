5f074540e8cfce0d402a17ab73bb8bb4f1837ea7
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-09 19:30:21 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <signal.h>
 #include "logs.h"
 #include "json_export.h"
 #include "utils.h"
-#include <readline/readline.h>
-#include <readline/history.h>
 
 #define ASCII_ART "\n" \
 ANSI_COLOR_MAGENTA " 888                       888    888  .d88888b.   .d8888b. " ANSI_COLOR_RESET "\n" \
@@ -21,104 +18,8 @@ ANSI_COLOR_MAGENTA "                       888" ANSI_COLOR_RESET "\n" \
 ANSI_COLOR_MAGENTA "                  Y8b d88P " ANSI_COLOR_RED " NO" ANSI_COLOR_LIGHT_GRAY "-nonsense digital forensics" ANSI_COLOR_RESET "\n" \
 ANSI_COLOR_MAGENTA "                   \"Y88P\"" ANSI_COLOR_RESET "\n"
 
-// Menu option definitions
-typedef enum {
-    MENU_LIVE_AUTH_LOG = 1,
-    MENU_LIVE_ERROR_LOG,
-    MENU_LIVE_LOG,
-    MENU_LIVE_NETWORK_LOG,
-    MENU_RUN_REGEX,
-    MENU_SEARCH_IP,
-    MENU_EDIT_LOG_PATHS,
-    MENU_EXPORT_JSON,
-    MENU_HELP,
-    MENU_EXIT
-} MenuOptions;
-
 // Global search paths
-char log_search_path[BUFFER_SIZE] = "/var/lib/docker /var/log";
-
-/**
- * Displays the help information for the tool.
- */
-void display_help() {
-    const char *help_text =
-        ANSI_COLOR_LIGHT_GRAY "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN "LogHOG " ANSI_COLOR_BLUE "is a " ANSI_COLOR_CYAN "FAST" ANSI_COLOR_BLUE " comprehensive log search tool with 2 modes\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_YELLOW "TAIL MODE\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "Logs are automatically stiched together by timestamp making \n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "events easy to follow in real time " ANSI_COLOR_CYAN "(CTRL+C to quit)\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_YELLOW "LESS MODE\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "Buffers from tail mode are sent directly to less, a powerful\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "text editing tool that allows for indepth review, searches and\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "real time log analysisi " ANSI_COLOR_CYAN "(h for help)" ANSI_COLOR_BLUE " or " ANSI_COLOR_CYAN "(q to quit)\n" ANSI_COLOR_RESET
-        ANSI_COLOR_LIGHT_GRAY "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN "MENU OVERVIEW\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "A" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "uthentication (Tail)\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN " - Identify events such as.\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'authentication failed'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'permission denied'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'invalid user'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'unauthorized access'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'SQL injection detected'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'cross-site scripting attempt'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'directory traversal attack'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'and more .. '\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "E" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "rrors (Tail)\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN " - Filters logs for error-related events.\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'error'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'failure'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'critical'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'socket timeout'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'network reset'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'DNS resolution failure'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'permission denied'\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "L" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "ive (LIVE, timestamp order)\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN " - Displays every log in real time, sorted by timestamp.\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "N" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "etwork Protocol Filter (Tail)\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN " - Filters logs by protocol.\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'http://'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'https://'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'ftp://'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'ssh://'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'telnet://'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'smtp://'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'sftp://'\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "R" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "egex (Tail)\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN " - Search EVERYTHING using standard regular expressions.\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'error|failure'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'REGEX_PATTERN'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '(?:[0-9]{1,3}\\.){3}[0-9]{1,3}'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '(authentication|permission|invalid user)'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '(DoS|DDoS attack)'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'brute-?force|directory traversal'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '(SQL injection|cross-site scripting)'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '(192.168.???.*) or ranges (192.168.[1..10].[1..100])\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '(GET|POST|PUT|DELETE|PATCH) /[a-zA-Z0-9/_-]*'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'cron.*\\((root|admin)\\)'\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "I" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "P (Log Search)\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN " - Filters logs by ip, ranges, and regular expressions.\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '(?:[0-9]{1,3}\\.){3}[0-9]{1,3}\'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '(192\\.168\\.[0-9]+\\.[0-9]+)'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '\\b(?:[A-Fa-f0-9]{1,4}:){7}[A-Fa-f0-9]{1,4}\\b'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '(::|(?:[A-Fa-f0-9]{1,4}:){1,7}:)'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     ':(?::[A-Fa-f0-9]{1,4}){1,7}'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '192\\.168\\.\\d{1,3}\\.\\d{1,3}'\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "S" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "et (Log Paths)\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN " - Allows setting custom log paths.\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '/var/log /opt/logs'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '/var/lib/docker /var/log/nginx'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     '/usr/local/logs /home/user/logs'\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "J" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "SON (Export tool)\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN " - Exports filtered logs to a JSON file in the home directory called log_search_results.json.\n" ANSI_COLOR_RESET
-        ANSI_COLOR_MAGENTA "     'log_search_results.json'\n" ANSI_COLOR_RESET
-        ANSI_COLOR_BLUE "     'jq '.[] | .log_entry' log_search_results.json\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "H" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "elp\n" ANSI_COLOR_RESET
-        ANSI_COLOR_CYAN " - Displays this Help.\n\n" ANSI_COLOR_RESET
-        ANSI_COLOR_DARK "[" ANSI_COLOR_LIGHT_GREEN "Q" ANSI_COLOR_DARK "]" ANSI_COLOR_BLUE "uit\n\n" ANSI_COLOR_RESET;
-    display_buffer_with_less(help_text, strlen(help_text));
-}
+char log_search_path[BUFFER_SIZE];
 
 /**
  * Displays the main menu and handles user input.
@@ -200,8 +101,10 @@ void main_menu() {
  * @return Exit status code.
  */
 int main() {
+    // Load initial log paths from the config file
+    load_log_paths(log_search_path, BUFFER_SIZE);
+
     signal(SIGINT, sigint_handler);
     main_menu();
     return 0;
 }
-
