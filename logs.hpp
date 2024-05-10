d7f991194d2d5b5864092bd5aa37713ab1355c5c
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-09 20:57:00 -0700

    Update logs.hpp

--- logs.hpp
+++ logs.hpp
@@ -1,12 +1,13 @@
+// logs.hpp
 #ifndef LOGS_HPP
 #define LOGS_HPP
 
-void live_auth_log(const char *log_search_path);
-void live_error_log(const char *log_search_path);
-void live_log(const char *log_search_path);
-void live_network_log(const char *log_search_path);
-void run_regex(const char *log_search_path);
-void search_ip(const char *log_search_path);
-void edit_log_paths(char *log_search_path);
+void live_auth_log(const char* log_search_path);
+void live_error_log(const char* log_search_path);
+void live_log(const char* log_search_path);
+void live_network_log(const char* log_search_path);
+void run_regex(const char* log_search_path);
+void search_ip(const char* log_search_path);
+void edit_log_paths(char* log_search_path);
 
-#endif
+#endif // LOGS_HPP
