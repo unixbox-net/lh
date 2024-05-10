14dc3fe350dee25a4c1cde2c1706371ef91674e1
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-09 20:21:46 -0700

    Update json_export.cpp

--- json_export.cpp
+++ json_export.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
 #include <json-c/json.h>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
 #include "json_export.hpp"
 #include "utils.hpp"
 
@@ -24,11 +23,14 @@ void export_search_results_to_json(const char *log_search_path) {
     json_object *json_arr = json_object_new_array();
     char buffer[BUFFER_SIZE];
     bool has_entries = false;
+    std::string all_entries;
 
-    while (fgets(buffer, BUFFER_SIZE, proc) != nullptr) {
+    while (fgets(buffer, sizeof(buffer), proc)) {
         json_object *json_obj = json_object_new_object();
         json_object_object_add(json_obj, "log_entry", json_object_new_string(buffer));
         json_object_array_add(json_arr, json_obj);
+
+        all_entries += buffer;
         has_entries = true;
     }
 
@@ -38,17 +40,18 @@ void export_search_results_to_json(const char *log_search_path) {
         const char *output_file = "log_search_results.json";
         FILE *output = fopen(output_file, "w");
         if (output) {
-            fputs(json_object_to_json_string_ext(json_arr, JSON_C_TO_STRING_PRETTY), output);
+            fputs(json_object_to_json_string(json_arr), output);
             fclose(output);
             std::cout << "\nExported search results to " << output_file << std::endl;
-
-            display_buffer_with_less(json_object_to_json_string_ext(json_arr, JSON_C_TO_STRING_PRETTY), strlen(json_object_to_json_string_ext(json_arr, JSON_C_TO_STRING_PRETTY)));
         } else {
             perror("fopen");
         }
+
+        display_buffer_with_less(all_entries.c_str(), all_entries.size());
     } else {
         std::cout << "\nNo matching log entries found.\n";
     }
 
     json_object_put(json_arr);
+    reset_menu_flag();
 }
