a0ffc677b9b640a967f2739ff5534e73c267e9b7
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-09 17:44:10 -0700

    Create logs.h

---
+++ logs.h
@@ -0,0 +1,15 @@
+#ifndef LOGS_H
+#define LOGS_H
+
+#define BUFFER_SIZE 4096
+
+void live_auth_log(const char *log_search_path);
+void live_error_log(const char *log_search_path);
+void live_log(const char *log_search_path);
+void live_network_log(const char *log_search_path);
+void run_regex(const char *log_search_path);
+void search_ip(const char *log_search_path);
+void edit_log_paths(char *log_search_path);
+
+#endif // LOGS_H
+
