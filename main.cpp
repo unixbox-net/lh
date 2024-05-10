603b83c34b25e5c3f3132dd96b1e5e94e63adab4
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-09 20:22:11 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <csignal>
 #include <iostream>
+#include <csignal>
+#include <unistd.h>
 #include "logs.hpp"
 #include "json_export.hpp"
 #include "utils.hpp"
@@ -19,35 +20,48 @@ void main_menu() {
                   << "8) Export to JSON\n"
                   << "9) Quit\n"
                   << "> ";
-        std::getline(std::cin, option);
 
-        if (option == "1") {
-            live_auth_log(log_search_path);
-        } else if (option == "2") {
-            live_error_log(log_search_path);
-        } else if (option == "3") {
-            live_log(log_search_path);
-        } else if (option == "4") {
-            live_network_log(log_search_path);
-        } else if (option == "5") {
-            run_regex(log_search_path);
-        } else if (option == "6") {
-            search_ip(log_search_path);
-        } else if (option == "7") {
-            edit_log_paths(log_search_path);
-        } else if (option == "8") {
-            export_search_results_to_json(log_search_path);
-        } else if (option == "9") {
-            exit(0);
-        } else {
-            std::cout << "Invalid option. Try again.\n";
+        std::getline(std::cin, option);
+        if (option.empty()) continue;
+
+        switch (option[0]) {
+            case '1':
+                live_auth_log(log_search_path);
+                break;
+            case '2':
+                live_error_log(log_search_path);
+                break;
+            case '3':
+                live_log(log_search_path);
+                break;
+            case '4':
+                live_network_log(log_search_path);
+                break;
+            case '5':
+                run_regex(log_search_path);
+                break;
+            case '6':
+                search_ip(log_search_path);
+                break;
+            case '7':
+                edit_log_paths(log_search_path);
+                break;
+            case '8':
+                export_search_results_to_json(log_search_path);
+                break;
+            case '9':
+                std::cout << "Exiting...\n";
+                exit(0);
+            default:
+                std::cerr << "Invalid option. Please try again.\n";
+                break;
         }
     }
 }
 
 int main() {
+    signal(SIGINT, sigint_handler);
     load_log_paths(log_search_path, BUFFER_SIZE);
-    std::signal(SIGINT, sigint_handler);
     main_menu();
     return 0;
 }
