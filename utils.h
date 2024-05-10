22df299411d84749e2e131ed3038a649b8016647
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-09 17:45:30 -0700

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,29 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#define ANSI_COLOR_RESET "\x1b[0m"
+#define ANSI_COLOR_RED "\x1b[31;1m"
+#define ANSI_COLOR_GREEN "\x1b[32;1m"
+#define ANSI_COLOR_LIGHT_GREEN "\x1b[92;1m"
+#define ANSI_COLOR_YELLOW "\x1b[33;1m"
+#define ANSI_COLOR_BLUE "\x1b[34;1m"
+#define ANSI_COLOR_MAGENTA "\x1b[35;1m"
+#define ANSI_COLOR_CYAN "\x1b[36;1m"
+#define ANSI_COLOR_WHITE "\x1b[37;1m"
+#define ANSI_COLOR_LIGHT_GRAY "\x1b[37;1m"
+#define ANSI_COLOR_DARK "\x1b[30m"
+#define ANSI_COLOR_BG "\x1b[48;5;235m"
+#define BUFFER_SIZE 4096
+
+void find_logs_command(char *buffer, size_t size, const char *search_path);
+void display_buffer_with_less(const char *buffer, size_t length);
+void run_command_with_buffer(const char *cmd, void (*buffer_action)(const char *, size_t));
+char *get_user_input(const char *prompt);
+int sanitize_input(char *input);
+void sigint_handler(int sig);
+
+#endif // UTILS_H
+
