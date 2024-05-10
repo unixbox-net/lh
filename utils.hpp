ce4c5278ac7ab3adbfe9a9c549b295605a2518c4
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-09 20:19:59 -0700

    Update utils.hpp

--- utils.hpp
+++ utils.hpp
@@ -3,18 +3,17 @@
 
 #include <string>
 
-#define BUFFER_SIZE 4096
-#define CONFIG_FILE "/etc/loghog.conf"
+const int BUFFER_SIZE = 4096;
+const char *CONFIG_FILE = "/etc/loghog.conf";
 
-// Function prototypes
 void find_logs_command(char *buffer, size_t size, const char *search_path);
 void display_buffer_with_less(const char *buffer, size_t length);
 void run_command_with_buffer(const char *cmd, void (*buffer_action)(const char *, size_t));
 void sigint_handler(int sig);
 std::string get_user_input(const std::string &prompt);
 bool sanitize_input(std::string &input);
-void reset_menu_flag();
 void save_log_paths(const char *log_search_path);
 void load_log_paths(char *log_search_path, size_t buffer_size);
+void reset_menu_flag();
 
 #endif
