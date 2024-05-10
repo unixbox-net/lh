277dadbb5547adf6a8c7a6944732229ee4280d88
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-09 21:18:47 -0700

    Update utils.hpp

--- utils.hpp
+++ utils.hpp
@@ -3,11 +3,13 @@
 
 #include <string>
 
-void find_logs_command(char* find_cmd, size_t buffer_size, const char* log_search_path);
-void run_command_with_buffer(const char* command, char* buffer);
-std::string get_user_input(const std::string& prompt);
-bool sanitize_input(std::string& input);
-void save_log_paths(const char* log_paths);
-std::string load_log_paths_from_config();
+#define BUFFER_SIZE 1024
+
+std::string get_user_input(const std::string &prompt);
+bool sanitize_input(std::string &input);
+void find_logs_command(char *buffer, size_t size, const char *log_paths);
+void run_command_with_buffer(const char *cmd, FILE *out = nullptr);
+void save_log_paths(const char *log_paths);
+void load_log_paths(char *log_paths, size_t buffer_size);
 
 #endif // UTILS_HPP
