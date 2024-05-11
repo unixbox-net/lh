72b3cce9f5b44af44522cf9d39c0917c0122008f
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-11 13:34:35 -0700

    Update rhel-8.sh

--- rhel-8.sh
+++ rhel-8.sh
@@ -5,12 +5,13 @@ BASE_DIR="/root/lh"
 RPM_BUILD_DIR="${BASE_DIR}/rpmbuild"
 PACKAGE_NAME="lh"
 VERSION="1.0.0"
-RPM_PACKAGE_PATH="${RPM_BUILD_DIR}/RPMS/x86_64/${PACKAGE_NAME}-${VERSION}-1.el8.x86_64.rpm"
 
 # Prepare environment function
 prepare_environment() {
     echo "Cleaning up previous builds..."
     rm -rf "${RPM_BUILD_DIR}"
+    #mkdir -p "${RPM_BUILD_DIR}/{BUILD,RPMS,SOURCES,SPECS,SRPMS}"
+
     echo "Installing necessary dependencies..."
     sudo dnf install -y rpm-build gcc json-c-devel readline-devel
 }
@@ -18,9 +19,19 @@ prepare_environment() {
 # Prepare source function
 prepare_source() {
     echo "Preparing source files..."
-    mkdir -p "${RPM_BUILD_DIR}/SOURCES/lh-${VERSION}" && chmod 755 -R "${RPM_BUILD_DIR}"
+    mkdir -p "${RPM_BUILD_DIR}/SOURCES/lh-${VERSION}"
+    mkdir -p /root/lh/rpmbuild && chmod 755 -R /root/lh/rpmbuild
+    mkdir -p /root/lh/rpmbuild/
+    mkdir -p /root/lh/rpmbuild/BUILD
+    mkdir -p /root/lh/rpmbuild/BUILDROOT
+    mkdir -p /root/lh/rpmbuild/RPMS
+    mkdir -p /root/lh/rpmbuild/SOURCES
+    mkdir -p /root/lh/rpmbuild/SPECS
+    mkdir -p /root/lh/rpmbuild/SRPMS
+    cp -r /root/lh/lh.spec /root/lh/rpmbuild/SPECS/
     cp -r "${BASE_DIR}/src/"* "${RPM_BUILD_DIR}/SOURCES/lh-${VERSION}/"
     cp "${BASE_DIR}/LICENSE" "${BASE_DIR}/README.md" "${RPM_BUILD_DIR}/SOURCES/lh-${VERSION}/"
+
     # Create tarball
     tar czf "${RPM_BUILD_DIR}/SOURCES/${PACKAGE_NAME}-${VERSION}.tar.gz" -C "${RPM_BUILD_DIR}/SOURCES/" lh-${VERSION}
     cp "${BASE_DIR}/${PACKAGE_NAME}.spec" "${RPM_BUILD_DIR}/SPECS/"
@@ -32,16 +43,9 @@ build_rpm() {
     rpmbuild -ba "${RPM_BUILD_DIR}/SPECS/${PACKAGE_NAME}.spec" --define "_topdir ${RPM_BUILD_DIR}"
 }
 
-# Install or Reinstall RPM function
-install_or_reinstall_rpm() {
-    echo "Checking if package is installed and taking appropriate action..."
-    if rpm -q ${PACKAGE_NAME} &>/dev/null; then
-        echo "Package is installed, attempting reinstall..."
-        sudo dnf reinstall -y ${RPM_PACKAGE_PATH}
-    else
-        echo "Package not installed, attempting install..."
-        sudo dnf install -y ${RPM_PACKAGE_PATH}
-    fi
+install_rpm() {
+    echo "Installing RPM package..."
+    sudo dnf reinstall-y "${RPM_BUILD_DIR}/RPMS/x86_64/${PACKAGE_NAME}-${VERSION}-1.el8.x86_64.rpm"
 }
 
 # Main function
@@ -49,7 +53,7 @@ main() {
     prepare_environment
     prepare_source
     build_rpm
-    install_or_reinstall_rpm
+    install_rpm
 }
 
 # Execute the main function
