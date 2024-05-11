a571688ab547f96840c2ee465db067d52726ae36
unixbox-net <anthony@unixbox.net>
2024-05-11 12:26:58 -0700

    Refactored build scripts and updated documentation

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
+git status  # Check which files changed
 git commit -m "Update deployment scripts and add lh directory"
 git push origin main
+git add .  # Stage all changes
+git commit -m "Refactored build scripts and updated documentation"
+git push origin main  # Push changes to the main branch on the remote
 
