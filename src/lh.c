5768dfae09c87a07fd254cc0a937d66418149f1b
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2024-05-14 13:23:42 -0700

    Update lh.c

--- src/lh.c
+++ src/lh.c
@@ -26,17 +26,17 @@
 #define ANSI_COLOR_BG "\x1b[48;5;235m"
 
 #define ASCII_ART \
-    ANSI_COLOR_LIGHT_GRAY "\n888                       888    888  .d88888b.   .d8888b. " ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "888                       888    888 d88P\" \"Y88b d88P  Y88b" ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "888                       888    888 888     888 888    888" ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "888      .d88b.   .d88b.  8888888888 888     888 888       " ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "888     d88\"\"88b d88P\"88b 888    888 888     888 888  88888" ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "888     888  888 888  888 888    888 888     888 888    888" ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "888     Y88..88P Y88b 888 888    888 Y88b. .d88P Y88b  d88P" ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "88888888 \"Y88P\"   \"Y88888 888    888  \"Y88888P\"   \"Y8888P88" ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "                       888" ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "                  Y8b d88P " ANSI_COLOR_RED " NO" ANSI_COLOR_LIGHT_GRAY " nonsense digital forensics" ANSI_COLOR_RESET "\n" \
-    ANSI_COLOR_LIGHT_GRAY "                   \"Y88P\"" ANSI_COLOR_RESET "\n"
+    ANSI_COLOR_CYAN "\n888                       888    888  .d88888b.   .d8888b. " ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "888                       888    888 d88P\" \"Y88b d88P  Y88b" ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "888                       888    888 888     888 888    888" ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "888      .d88b.   .d88b.  8888888888 888     888 888       " ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "888     d88\"\"88b d88P\"88b 888    888 888     888 888  88888" ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "888     888  888 888  888 888    888 888     888 888    888" ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "888     Y88..88P Y88b 888 888    888 Y88b. .d88P Y88b  d88P" ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "88888888 \"Y88P\"   \"Y88888 888    888  \"Y88888P\"   \"Y8888P88" ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "                       888" ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "                  Y8b d88P " ANSI_COLOR_RED " NO" ANSI_COLOR_LIGHT_GRAY " nonsense digital forensics" ANSI_COLOR_RESET "\n" \
+    ANSI_COLOR_CYAN "                   \"Y88P\"" ANSI_COLOR_RESET "\n"
 
 typedef enum {
     MENU_LIVE_AUTH_LOG = 1,
